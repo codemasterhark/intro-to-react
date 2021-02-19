3cb5284ec3c7fcd230d33524e6d2e16a34ae0fd2
codemasterhark <codemasterhark@gmail.com>
2021-02-19 23:40:59 +0530

    Added JSX, updated eslint config

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import ReactDOM from "react-dom";
 import Video from "./Video";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, "WeTube"),
-    React.createElement(Video, {
-      title: "The best Video",
-      dateAdded: "2 days ago",
-      channel: "News Channel",
-    }),
-    React.createElement(Video, {
-      title: "The cool Video",
-      dateAdded: "3 days ago",
-      channel: "Local Channel",
-    }),
-    React.createElement(Video, {
-      title: "The learning Video",
-      dateAdded: "6 days ago",
-      channel: "Learning Channel",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>WeTube</h1>
+      <Video
+        title="The Best Video"
+        dateAdded="2 days ago"
+        channel="News Channel"
+      />
+      <Video
+        title="Cool Video Title"
+        dateAdded="5 days ago"
+        channel="Local Channel"
+      />
+      <Video
+        title="Learning Video Title"
+        dateAdded="1 week ago"
+        channel="Learning Channel"
+      />
+    </div>
+  );
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
