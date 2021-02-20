b21da35df140344c5626b125ee856ca75b934001
codemasterhark <codemasterhark@gmail.com>
2021-02-20 19:41:42 +0530

    error boundary with time setting added

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { Link } from "@reach/router";
+import { Link, Redirect } from "@reach/router";
 
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, redirect: false };
   }
   static getDerivedStateFromError() {
     return { hasError: true };
@@ -12,12 +12,21 @@ class ErrorBoundary extends Component {
   componentDidCatch(error, info) {
     console.error("ErrorBoundary caught an error", error, info);
   }
+  componentDidUpdate() {
+    if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 6000);
+    }
+  }
   render() {
+    if (this.state.redirect) {
+      return <Redirect to="/" noThrow />;
+    }
     if (this.state.hasError) {
       return (
         <h1>
-          There was an error while fetching the video <Link to="/">Click here</Link>{" "}
-          to back to the home page
+          There was an error while fetching the video{" "}
+          <Link to="/">Click here</Link> to back to the home page or wait 6
+          seconds.
         </h1>
       );
     }
