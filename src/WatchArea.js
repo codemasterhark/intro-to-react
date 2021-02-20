26c3a42b2c8c03d42e5e30ce4afdbcccfca4a87c
codemasterhark <codemasterhark@gmail.com>
2021-02-20 18:44:08 +0530

    Class component in watch area, loading indicator

--- src/WatchArea.js
+++ src/WatchArea.js
@@ -1,7 +1,65 @@
 import React from "react";
+import axios from "axios";
 
-const WatchArea = (props) => {
-  return <h1>Hello from Watch page... id is {props.id}</h1>;
-};
+class WatchArea extends React.Component {
+  constructor() {
+    super();
+    this.state = { loading: true };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://www.googleapis.com/youtube/v3/videos?key=${process.env.API_KEY}&part=snippet,contentDetails,statistics,status,player&id=${this.props.id}`
+      )
+      .then((res) => {
+        const item = res.data.items[0];
+        this.setState({
+          title: item.snippet.title,
+          views: item.statistics.viewCount,
+          description: item.snippet.description,
+          channel: item.snippet.channelTitle,
+          like: item.statistics.likeCount,
+          loading: false,
+          url: item.id,
+        });
+      })
+      .catch((err) => this.setState({ error: err }));
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1 className="loader">loading … </h1>;
+    }
+
+    const { title, views, description, channel, like, url } = this.state;
+
+    return (
+      <div className="watch-area">
+        <div>
+          <div className="player">
+            <iframe
+              title={title}
+              width="1050"
+              height="450"
+              src={`//www.youtube.com/embed/${url}`}
+              frameBorder="0"
+              allow=" autoplay encrypted-media; "
+              allowFullScreen
+            ></iframe>
+          </div>
+          <h1>{title}</h1>
+          <div className="video-info">
+            <div>{views} Views</div>
+            <div>{like} Likes</div>
+          </div>
+          <div className="channel-name">{channel} Channel</div>
+
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default WatchArea;
